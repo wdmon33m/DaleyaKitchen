df27c4dcf3ffba9efd5a05f11739b5095acce864
wdmon33m <wdmon33m@gmail.com>
2023-10-15 00:13:32 -0500

    Create the project

---
+++ Daleya.WEB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Daleya.WEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
